57bc191e378592d11c90f0edfe12c380d5abac54
Alexandre Moreira <brainmunchers@gmail.com>
2022-07-15 23:50:11 +0200

    add dice sprite, onhover scale effect, random number assigned and displayed to di

--- Assets/Scripts/Di.cs
+++ Assets/Scripts/Di.cs
@@ -5,14 +5,41 @@ using TMPro;
 
 public class Di : MonoBehaviour {
 
-    public TextMeshProUGUI valueText;
+    public TMP_Text valueText;
     public int value;
-
+    
+    private Color startcolor;
+    private float newScale;
+    
     private void Start() {
         value = Random.Range(1, 7);
+        newScale = 0.15f;
     }
 
     private void Update() {
+        RollDi();
+    }
+
+    void OnMouseEnter()
+    {
+        print($"Mouse entered {this.name}");
+        // float newScale = Mathf.Lerp(0.1f, 0, Time.deltaTime / 10);
+        // print(newScale);
+        transform.localScale += new Vector3(newScale, newScale, 0);
+    }
+    
+    void OnMouseExit()
+    {
+        // float newScale = Mathf.Lerp(0, 0.1f, Time.deltaTime / 10);
+        transform.localScale -= new Vector3(newScale, newScale, 0);
+    }
+    
+    void OnMouseDown()
+    {
+        print($"selected {this.value} as attack");
+    }
+    
+    void RollDi() {
         valueText.text = value.ToString();
     }
 }
